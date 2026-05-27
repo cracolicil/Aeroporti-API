ab563edd06f6046be1411c8e5ba998b3cd62ef70
Leonardo Cracolici <leonardo.cracolici@gmail.com>
2026-05-27 15:21:36 +0200

    creato test per aeroporto not found

--- tests/test_airport.py
+++ tests/test_airport.py
@@ -14,9 +14,13 @@ def test_get_airports_default_pagination():
     assert isinstance(data["data"], list)
     assert len(data["data"]) >= 1
 
-def test_get_books_with_pagination():
+def test_get_airports_with_pagination():
     response = client.get("/aeroporti?pages=1&size=1")
     assert response.status_code == 200
 
     data = response.json()
     assert len(data["data"]) == 1
+
+def test_get_airport_not_found():
+    response = client.get("/aeroporti/999")
+    assert response.status_code == 404
