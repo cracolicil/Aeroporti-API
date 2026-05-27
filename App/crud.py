4a5a29bed208d56964d2eb319ad4105e160c701f
Leonardo Cracolici <leonardo.cracolici@gmail.com>
2026-05-27 12:44:12 +0200

    Creata funzione get per una lista di aeroporti con attributi pagina e size

--- App/crud.py
+++ App/crud.py
@@ -2,3 +2,9 @@ import logging
 from .database import aeroporti_db
 
 logger = logging.getLogger(__name__)
+
+def get_aeroporti(page: int, size: int):
+    logger.info(f"Fetching airports page={page} size={size}")
+    start = (page - 1) * size
+    end = start + size
+    return aeroporti_db[start:end]
