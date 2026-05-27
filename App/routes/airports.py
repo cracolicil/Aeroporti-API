bbf24dd7b7f562c95987601ef6ed491c8c692a9b
Leonardo Cracolici <leonardo.cracolici@gmail.com>
2026-05-27 14:12:42 +0200

    creata funzione che mostra una lista di aeroporti con tag GET

--- App/routes/airports.py
+++ App/routes/airports.py
@@ -4,3 +4,6 @@ from .. import crud
 from ..models import Aeroporto, AeroportoBase
 
 router = APIRouter(prefix="/aeroporti", tags=["Aeroporti"])
+@router.get("", response_model=List[Aeroporto])
+def list_airports(page: int = 1, size = 10):
+    return crud.get_aeroporti(page, size)
