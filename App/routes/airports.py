847ac5c5ea93cea925d8bfc4df4d91224c84290a
Leonardo Cracolici <leonardo.cracolici@gmail.com>
2026-05-27 14:15:07 +0200

    creata funzione che mostra un aeroporto indicando uno specifico id, con tag GET

--- App/routes/airports.py
+++ App/routes/airports.py
@@ -7,3 +7,10 @@ router = APIRouter(prefix="/aeroporti", tags=["Aeroporti"])
 @router.get("", response_model=List[Aeroporto])
 def list_airports(page: int = 1, size = 10):
     return crud.get_aeroporti(page, size)
+
+@router.get("/{airport_id}", response_model=Aeroporto)
+def get_airport(airport_id: int):
+    airport = crud.get_aeroporto(airport_id)
+    if not airport:
+        raise HTTPException(status_code=404, detail="Airport not found")
+    return airport
