99c312c2009b26cfb0f970cc91a3de52c0e4fa84
Leonardo Cracolici | Fulcri <leonardocracolici@fulcri.it>
2026-06-29 17:02:09 +0200

    corretta funziona bash docker build per far si che legga l'intera directory e non solo il Dockerfile

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,7 +1,7 @@
 # Richiede Docker
 
 # Builda l'immagine docker
-docker build --no-cache -t leocraco/aeroporti:v1 - < Dockerfile
+docker build --no-cache -t leocraco/aeroporti:v1 .
 
 # Facciamo partire un container
 CONTAINER_ID = $(docker run leocraco/aeroporti:v1)
